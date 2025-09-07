44a0da72103f9ca39eabce973f6f4b065426236e
leandro.brito <leandro.brito@memora.com.br>
2025-09-07 02:36:09 -0300

    feat:(security):adicionando UserPrincipal para integração do usuário com o spring Security

---
+++ src/main/java/com/estagiarios/e_commerce/security/UserPrincipal.java
@@ -0,0 +1,68 @@
+package com.estagiarios.e_commerce.security;
+
+import com.estagiarios.e_commerce.entity.Role;
+import com.estagiarios.e_commerce.entity.Usuario;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Data
+@AllArgsConstructor
+public class UserPrincipal implements UserDetails {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String senha;
+    private Set<Role> roles;
+    public static  UserPrincipal create(Usuario usuario){
+        return new UserPrincipal(
+                usuario.getId(),
+                usuario.getNome(),
+                usuario.getCpf(),
+                usuario.getEmail(),
+                usuario.getSenha(),
+                usuario.getRoles()
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+    @Override
+    public String getUsername() {
+        return cpf;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
